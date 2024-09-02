f5844778694bd3c780fc37176fb97070e2924146
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-02 19:37:13 -0300

    finalizado as fuções da controller e rotas

--- api/src/controller/taskController.js
+++ api/src/controller/taskController.js
@@ -1,3 +1,4 @@
+const { task } = require('../libs/prismaLib');
 const taskServices = require('../services/taskServices');
 
 const State = {
@@ -32,6 +33,91 @@ class TaskController {
       const response = await taskServices.addNewTask(title, description, state);
 
       res.status(201).json(response);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'Algo deu errado.', error: JSON.stringify(error) });
+    }
+  }
+  async readAll(_, res) {
+    try {
+      const response = await taskServices.getAllTask();
+
+      res.status(200).json(response);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'Algo deu errado.', error: JSON.stringify(error) });
+    }
+  }
+  async update(req, res) {
+    try {
+      const { taskId } = req.query;
+      const { title, description, state } = req.body;
+
+      if (typeof taskId === 'number' && taskId > 0) {
+        return res
+          .status(409)
+          .json({ error: 'Id da tarefa é obrigatório, e deve ser um número' });
+      }
+
+      if (!title || typeof title !== 'string') {
+        return res
+          .status(409)
+          .json({ error: 'O titulo é obrigatório, e deve ser texto' });
+      }
+
+      if (!description || typeof description !== 'string') {
+        return res
+          .status(409)
+          .json({ error: 'A descrição é obrigatório, e deve ser texto' });
+      }
+
+      if (!Object.values(State).includes(state)) {
+        return res.status(409).json({
+          error: 'O status é obrigatório e deve ser "TODO", "DOING" ou "DONE"',
+        });
+      }
+
+      const response = await taskServices.updateTask(
+        taskId,
+        title,
+        description,
+        state
+      );
+
+      if (!response) {
+        return res.status(400).json({
+          error: 'Tarefa não existe',
+        });
+      }
+
+      res.status(200).json({ message: 'Tarefa atualizada com succeso.' });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'Algo deu errado.', error: JSON.stringify(error) });
+    }
+  }
+  async delete(req, res) {
+    try {
+      const { taskId } = req.query;
+
+      if (typeof taskId === 'number' && taskId > 0) {
+        return res
+          .status(409)
+          .json({ error: 'Id da tarefa é obrigatório, e deve ser um número' });
+      }
+
+      const response = await taskServices.deleteTask(taskId);
+
+      if (!response) {
+        return res.status(400).json({
+          error: 'Tarefa não existe',
+        });
+      }
+
+      res.status(200).json({ message: 'Tarefa deletada com succeso.' });
     } catch (error) {
       res
         .status(500)
--- api/src/routes/taskRoute.js
+++ api/src/routes/taskRoute.js
@@ -4,5 +4,8 @@ const taskController = require('../controller/taskController');
 const taskRoute = express.Router();
 
 taskRoute.post('/create', taskController.create);
+taskRoute.get('/read-all', taskController.readAll);
+taskRoute.put('/update', taskController.update);
+taskRoute.delete('/delete', taskController.delete);
 
 module.exports = taskRoute;
