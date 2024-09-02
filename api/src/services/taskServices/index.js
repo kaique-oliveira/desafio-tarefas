7bc7e7a2c7009a0735fecc520bf7d62d6dab66fc
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-02 19:36:50 -0300

    finalizado as funções de crud

--- api/src/services/taskServices/index.js
+++ api/src/services/taskServices/index.js
@@ -8,16 +8,66 @@ class TaskServices {
         data: { title, description, state },
       });
 
-      console.log(res);
       return res;
     } catch (error) {
-      console.log(error);
       throw error;
     }
   }
-  getAllTask() {}
-  updateTask() {}
-  deleteTask() {}
+  async getAllTask() {
+    try {
+      const res = await dbTaks.task.findMany({ orderBy: { title: 'asc' } });
+
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updateTask(taskId, title, description, state) {
+    try {
+      const oldTask = await this.getOneById(taskId);
+
+      if (!oldTask) {
+        return false;
+      }
+
+      await dbTaks.task.update({
+        data: { title, description, state },
+        where: { id: Number(taskId) },
+      });
+
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getOneById(taskId) {
+    try {
+      const res = await dbTaks.task.findUnique({
+        where: { id: Number(taskId) },
+      });
+
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deleteTask(taskId) {
+    try {
+      const oldTask = await this.getOneById(taskId);
+
+      if (!oldTask) {
+        return false;
+      }
+
+      await dbTaks.task.delete({
+        where: { id: Number(taskId) },
+      });
+
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 const taskServices = new TaskServices();
