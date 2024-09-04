d3b9e38331538130e50b312302b6f591211ba75a
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-04 08:30:17 -0300

    Adicionado os providers dos hooks

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,27 @@
-import { ThemeProvider } from 'styled-components';
 import { Taks } from './pages/Task';
-import theme from './theme';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { DialogProvider } from './context/dialogContext';
+import { AlertProvider } from './context/alertContext';
+import { LoadingProvider } from './context/loadingContext';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Taks />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <DialogProvider>
+        <AlertProvider>
+          <LoadingProvider>
+            <Taks />
+          </LoadingProvider>
+        </AlertProvider>
+      </DialogProvider>
     </ThemeProvider>
   );
 }
