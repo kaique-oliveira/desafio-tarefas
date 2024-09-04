f600cf5600ae19d5a9c0aeb1ae9c8dbc7ad353a7
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-04 10:19:01 -0300

    url api produção

--- web/src/libs/apiTask.js
+++ web/src/libs/apiTask.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const apiTask = axios.create({
-  //baseURL: 'https://royal-api-desenvolvimento.sete.digital/api',
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'http://159.89.239.199:3001/api',
+  //baseURL: 'http://localhost:3001/api',
 });
