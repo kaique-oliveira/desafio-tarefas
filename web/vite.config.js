a7bbe95c084c31fd15ef1d8a366fa8dc6d8a7c8c
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-04 12:55:54 -0300

    Ajuste do vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
+    host: '0.0.0.0', // Permite acesso externo
+    port: 3000, // Define a porta que o servidor irá escutar
+  },
+  preview: {
+    host: '0.0.0.0', // Permite acesso externo no modo preview
+    port: 3000, // Define a porta que o servidor irá escutar no modo preview
   },
 });
